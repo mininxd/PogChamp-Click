7e051ff85b3cc0e63e0e905f5da34b16bb7ff894
Minin <70429604+MininxD@users.noreply.github.com>
2021-09-03 16:17:26 +0700

    Mengurangi achievement

--- achievement.js
+++ achievement.js
@@ -32,12 +32,12 @@ className:"toastify-default",
 	}
 if(angka === 50) {
 	var newbie = document.getElementById("starter");
-	  starter.setAttribute("src", "trophy/bronze.png");
+	  starter.setAttribute("src", "trophy/silver.png");
   document.getElementById("starter-text").innerHTML = "Starter";
 	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/bronze.png",
+		avatar:"trophy/silver.png",
 className:"toastify-default",
   text: "Starter",
   duration: 5000,
@@ -48,12 +48,12 @@ className:"toastify-default",
 	//200
 	if(angka === 200) {
 		var tap = document.getElementById("taptap");
-	  tap.setAttribute("src", "trophy/bronze.png");
+	  tap.setAttribute("src", "trophy/gold.png");
   document.getElementById("taptap-text").innerHTML = "Tap tap tap";
 	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/bronze.png",
+		avatar:"trophy/gold.png",
 className:"toastify-default",
   text: "Tap tap tap",
   duration: 5000,
@@ -64,12 +64,12 @@ className:"toastify-default",
 	//500
 if(angka === 500) {
 	var fast = document.getElementById("urfast");
-	  fast.setAttribute("src", "trophy/bronze.png");
+	  fast.setAttribute("src", "trophy/platinum.png");
   document.getElementById("urfast-text").innerHTML = "ur fast...";
 	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/bronze.png",
+		avatar:"trophy/platinum.png",
 className:"toastify-default",
   text: "ur fast...",
   duration: 5000,
@@ -77,7 +77,7 @@ className:"toastify-default",
   position: "center", 
   }).showToast();
 	}
-if(angka === 500) {
+if(angka === 550) {
 	 	var rick = new Audio('rickroll.mp3');
             	rick.play();
 	Toastify({
@@ -89,178 +89,13 @@ className:"toastify-rickroll",
   }).showToast();
   rick.currentTime = 0;
 	}
-if(angka === 1100) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/bronze.png",
-className:"toastify-default",
-  text: "Be careful",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 5000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/silver.png",
-className:"toastify-default",
-  text: "That sick",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 7000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/silver.png",
-className:"toastify-default",
-  text: "Screen Destroyer",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 7500) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/silver.png",
-className:"toastify-long-text",
-  text: "I think you love this game",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 10000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/gold.png",
-className:"toastify-default",
-  text: "The Great Clicker",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 12000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/gold.png",
-className:"toastify-default",
-  text: "holy sheesssh",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 16000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/gold.png",
-className:"toastify-default",
-  text: "Ok gamer",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 20000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/gold.png",
-className:"toastify-default",
-  text: "POG POG POG",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 50000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-default",
-  text: "Still playing?",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 100000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-default",
-  text: "Aw man...",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 500000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-default",
-  text: "Super!!!",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 1000001) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-long-text",
-  text: "Congrats, u r milioner",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 500000) {
-	achievement.play();
-	Toastify({
-		
-className:"toastify-default",
-  text: "No PowerUp",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-if(angka === 10000000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-default",
-  text: "this is the last",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
-	if(angka === 50000000) {
-	achievement.play();
-	Toastify({
-		avatar:"trophy/platinum.png",
-className:"toastify-default",
-  text: "seriously man",
-  duration: 5000,
-  gravity: "bottom", 
-  position: "center", 
-  }).showToast();
-	}
 if(angka >= 999999999) {
 	achievement.play();
 	Toastify({
 		avatar:"trophy/platinum.png",
 className:"toastify-default",
   text: "POGGERS!",
-  duration: 400,
+  duration: 100,
   gravity: "bottom", 
   position: "center", 
 }).showToast();
