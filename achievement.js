613c68cb35c48ea725c51848ecbc77a33787c8bd
Minin <70429604+MininxD@users.noreply.github.com>
2022-01-06 09:41:09 +0700

    animated score

--- achievement.js
+++ achievement.js
@@ -3,10 +3,9 @@ function angka_count() {
             angka = Number(localStorage.getItem("angka"));
             console.log(angka);
             angka += 1;
-            localStorage.setItem("achievement",document.getElementById("newbie-text"));             
-            localStorage.setItem("achievement",newbie);             
             localStorage.setItem("angka",angka);             
             document.getElementById("score").innerHTML = angka;
+
 if(angka === 1) {
 	Toastify({
 className:"toastify-default",
@@ -17,9 +16,6 @@ className:"toastify-default",
   }).showToast();
 	}
 if(angka === 10) {
-	var newbie = document.getElementById("newbie");
-	  newbie.setAttribute("src", "trophy/bronze.png");
-  document.getElementById("newbie-text").innerHTML = "Newbie";
 	achievement.play();
 	Toastify({
 		avatar:"trophy/bronze.png",
@@ -31,13 +27,9 @@ className:"toastify-default",
 }).showToast();
 	}
 if(angka === 50) {
-	var newbie = document.getElementById("starter");
-	  starter.setAttribute("src", "trophy/silver.png");
-  document.getElementById("starter-text").innerHTML = "Starter";
-	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/silver.png",
+		avatar:"trophy/bronze.png",
 className:"toastify-default",
   text: "Starter",
   duration: 5000,
@@ -45,15 +37,10 @@ className:"toastify-default",
   position: "center", 
 }).showToast();
 	}
-	//200
 	if(angka === 200) {
-		var tap = document.getElementById("taptap");
-	  tap.setAttribute("src", "trophy/gold.png");
-  document.getElementById("taptap-text").innerHTML = "Tap tap tap";
-	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/gold.png",
+		avatar:"trophy/bronze.png",
 className:"toastify-default",
   text: "Tap tap tap",
   duration: 5000,
@@ -61,15 +48,10 @@ className:"toastify-default",
   position: "center", 
   }).showToast();
 	}
-	//500
 if(angka === 500) {
-	var fast = document.getElementById("urfast");
-	  fast.setAttribute("src", "trophy/platinum.png");
-  document.getElementById("urfast-text").innerHTML = "ur fast...";
-	
 	achievement.play();
 	Toastify({
-		avatar:"trophy/platinum.png",
+		avatar:"trophy/bronze.png",
 className:"toastify-default",
   text: "ur fast...",
   duration: 5000,
@@ -77,7 +59,7 @@ className:"toastify-default",
   position: "center", 
   }).showToast();
 	}
-if(angka === 550) {
+if(angka === 500) {
 	 	var rick = new Audio('rickroll.mp3');
             	rick.play();
 	Toastify({
@@ -89,13 +71,178 @@ className:"toastify-rickroll",
   }).showToast();
   rick.currentTime = 0;
 	}
+if(angka === 1100) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/bronze.png",
+className:"toastify-default",
+  text: "Be careful",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 5000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/silver.png",
+className:"toastify-default",
+  text: "That sick",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 7000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/silver.png",
+className:"toastify-default",
+  text: "Screen Destroyer",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 7500) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/silver.png",
+className:"toastify-long-text",
+  text: "I think you love this game",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 10000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/gold.png",
+className:"toastify-default",
+  text: "The Great Clicker",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 12000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/gold.png",
+className:"toastify-default",
+  text: "holy sheesssh",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 16000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/gold.png",
+className:"toastify-default",
+  text: "Ok gamer",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 20000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/gold.png",
+className:"toastify-default",
+  text: "POG POG POG",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 50000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-default",
+  text: "Still playing?",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 100000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-default",
+  text: "Aw man...",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 500000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-default",
+  text: "Super!!!",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 1000001) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-long-text",
+  text: "Congrats, u r milioner",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 500000) {
+	achievement.play();
+	Toastify({
+		
+className:"toastify-default",
+  text: "No PowerUp",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+if(angka === 10000000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-default",
+  text: "this is the last",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
+	if(angka === 50000000) {
+	achievement.play();
+	Toastify({
+		avatar:"trophy/platinum.png",
+className:"toastify-default",
+  text: "seriously man",
+  duration: 5000,
+  gravity: "bottom", 
+  position: "center", 
+  }).showToast();
+	}
 if(angka >= 999999999) {
 	achievement.play();
 	Toastify({
 		avatar:"trophy/platinum.png",
 className:"toastify-default",
   text: "POGGERS!",
-  duration: 100,
+  duration: 400,
   gravity: "bottom", 
   position: "center", 
 }).showToast();
--- script.js
+++ script.js
@@ -3,17 +3,23 @@ let angka= 0;
 	let achievement = new Audio('achievement.mp3');
 
 	  function click_pog() {
-		document.getElementById("tombolA").style.display = "block";
-   document.getElementById("tombolL").style.display = "block";
             pog.currentTime = 0;
         var pog_click = document.getElementById('pog_image');
   pog_click.setAttribute("src", "1.png");
+  document.getElementById('score').style.display = "none";
 }
         function release() {
   var pog_reset = document.getElementById("pog_image");
   pog_reset.setAttribute("src", "2.png");
+  document.getElementById('score').style.display = "block";
 }
-        
+        function load()  {
+        	 if(localStorage.getItem("counter")!=null)
+                 {
+                     counter = Number(localStorage.getItem("counter"));
+                     document.getElementById("counterValue").innerHTML = counter;
+                 }
+           }
 	function maintheme() {
 	var sample = document.getElementById("foobar");
 sample.play();
@@ -29,21 +35,4 @@ className:"toastify-default",
   }).showToast();
         }
         
-        
-      function load()  {
-      	document.getElementById("tombolA").style.display = "none";
-      	document.getElementById("tombolL").style.display = "none";
-        	 if(localStorage.getItem("angka")!=null)
-                 {
-                     counter = Number(localStorage.getItem("angka"));
-                     document.getElementById("counterValue").innerHTML = counter;
-                 }
-}
-
-
-
-
-
-
-
-        
+  
